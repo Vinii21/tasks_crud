3159faf7353b85fcc2fdb71225cfb9d50ec9757c
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-05-11 09:32:50 -0600

    Express static, views con ejs, estilos de ruta principal

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const TODOS = require("./models/todos.models")
 
 const app = express()
 
+app.set("view engine", "ejs")
+app.use(express.static("public"), express.json())
+
 db.authenticate()
     .then(()=>console.log("Base de datos Conectada"))
     .catch(error=>console.error(error))
@@ -16,7 +19,72 @@ db.sync()
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res)=>{
-    res.send("Servidor iniciado")
+    res.render("index")
+})
+
+app.get("/api/v1/todos", async (req, res)=>{
+    try{
+       const todos = await TODOS.findAll({
+        attributes: {
+            exclude: ["createdAt","updatedAt"]
+        }
+       });
+       res.json(todos)
+    } catch (error) {
+        res.status(400).json(err)
+    }
+})
+
+app.get("/api/v1/todos/:id", async (req, res)=>{
+    try{
+        const {id} = req.params;
+        const todo = await TODOS.findByPk(id, {
+            attributes: {
+                exclude: ["createdAt","updatedAt"]
+            }
+        })
+        res.json(todo)
+    } catch (error) {
+        res.status(400).json(err)
+    }
+})
+
+app.post("/api/v1/todos", async (req, res)=>{
+    try{
+        const newTodo = req.body;
+        await TODOS.create(newTodo);
+        res.status(201).send()
+    } catch (error) {
+        res.status(400).json(err)
+    }
+})
+
+app.put("/api/v1/todos/:id", async (req, res)=>{
+    try{
+        const {id} = req.params;
+        const {title, description, completed} = req.body;
+        await TODOS.update(
+            {title, description, completed},
+            {
+                where: {id}
+            }
+        );
+        res.status(204).send();
+    } catch (error) {
+        res.status(400).json(err)
+    }
+})
+
+app.delete("/api/v1/todos/:id", async (req, res)=>{
+    try{
+        const {id} = req.params;
+        await TODOS.destroy({
+            where: {id}
+        })
+        res.status(204).send()
+    } catch (error) {
+        res.status(400).json(error)
+    }
 })
 
 app.listen(PORT, ()=>{
